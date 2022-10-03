87a79622b4ab1de0e812fb6ad8fa8ed449631820
Julio Cesar Gaviria J <jcgj56@hotmail.com>
2022-10-03 12:27:25 -0500

    Was updated the library with null safety

--- lib/src/input_code_field.dart
+++ lib/src/input_code_field.dart
@@ -16,7 +16,7 @@ class InputCodeControl extends ChangeNotifier {
   bool get hasFocus => focusNode.hasFocus;
 
   /// Text and Action input.
-  TextInputConnection _connection;
+  TextInputConnection? _connection;
 
   /// Next index to fill.
   int _activeIndex = 0;
@@ -25,17 +25,17 @@ class InputCodeControl extends ChangeNotifier {
   int get activeIndex => _activeIndex;
 
   /// Current text value - an input state.
-  TextEditingValue _value = TextEditingValue();
+  TextEditingValue? _value = TextEditingValue();
 
   /// Current text value with correct cursor position.
-  TextEditingValue get _valueCursor =>
-      _value.copyWith(selection: TextSelection.collapsed(offset: activeIndex));
+  TextEditingValue? get _valueCursor =>
+      _value?.copyWith(selection: TextSelection.collapsed(offset: activeIndex));
 
   /// Returns current text value.
-  String get value => _value.text ?? '';
+  String get value => _value?.text ?? '';
 
   /// Sets current text value.
-  set value(String code) => _updateText(code);
+  set value(String? code) => _updateText(code);
 
   /// Number of fields.
   int _count = 0;
@@ -56,22 +56,22 @@ class InputCodeControl extends ChangeNotifier {
   bool get inputActive => _connection?.attached ?? false;
 
   /// Callback when last field is filled.
-  VoidCallback _done;
+  late VoidCallback _done;
 
   /// String regex to filter input. Matching test is applied at whole text value.
   /// So use ^[0-9]*$ to match only numbers.
-  String inputRegex;
+  String? inputRegex;
 
   /// Obscure input fields.
-  bool _obscure;
+  bool? _obscure;
 
   /// Checks if input fields are obscured and text should be hidden.
-  bool get isObscured => _obscure;
+  bool? get isObscured => _obscure;
 
   /// Controls [InputCodeField], receives input from keyboard and handles value editing.
   /// [code] - Initial value.
   /// [inputRegex] - String regex to filter input. Matching test is applied at whole text value.
-  InputCodeControl({String code, this.inputRegex}) {
+  InputCodeControl({String? code, this.inputRegex}) {
     if (code != null) {
       _value = TextEditingValue(text: code);
       _activeIndex = code.length;
@@ -98,12 +98,12 @@ class InputCodeControl extends ChangeNotifier {
   }
 
   /// Validates input [value] with [inputRegex].
-  bool validateInput(String value) {
+  bool validateInput(String? value) {
     if (inputRegex == null) {
       return true;
     }
 
-    return RegExp(inputRegex).hasMatch(value ?? '');
+    return RegExp(inputRegex!).hasMatch(value ?? '');
   }
 
   /// Registers [VoidCallback] that is triggered when last field is filled.
@@ -124,7 +124,7 @@ class InputCodeControl extends ChangeNotifier {
 
   /// Updates [TextEditingValue] with given [text] and sets cursor to correct position.
   /// If input validation fails and connection is assembled, previous value is send to input client.
-  void _updateText(String text) {
+  void _updateText(String? text) {
     if (text == null) {
       _updateValue(TextEditingValue());
     } else {
@@ -136,7 +136,7 @@ class InputCodeControl extends ChangeNotifier {
     }
 
     if (inputActive) {
-      _connection?.setEditingState(_valueCursor);
+      _connection?.setEditingState(_valueCursor!);
     }
   }
 
@@ -147,7 +147,7 @@ class InputCodeControl extends ChangeNotifier {
       return;
     }
 
-    if (_value.text == value.text) {
+    if (_value?.text == value.text) {
       _value = value;
       return;
     }
@@ -155,7 +155,7 @@ class InputCodeControl extends ChangeNotifier {
     if (value.text.length > _count || !validateInput(value.text)) {
       // Seems like duplicate call, but value can be updated directly by TextInputClient.
       if (inputActive) {
-        _connection?.setEditingState(_valueCursor);
+        _connection?.setEditingState(_valueCursor!);
       }
 
       return;
@@ -172,7 +172,7 @@ class InputCodeControl extends ChangeNotifier {
   }
 
   /// Executes [done] callback.
-  void _onDone() => _done?.call();
+  void _onDone() => _done.call();
 
   /// Removes focus from [focusNode].
   /// Check [FocusNode.unfocus].
@@ -194,10 +194,10 @@ class InputCodeControl extends ChangeNotifier {
 
   /// Helper function to copy/paste value from System clipboard.
   Future<void> copyFromClipboard() async {
-    ClipboardData data = await Clipboard.getData(Clipboard.kTextPlain);
+    ClipboardData? data = await Clipboard.getData(Clipboard.kTextPlain);
 
     if (data != null && validateInput(data.text)) {
-      value = data.text;
+      value = data.text!;
     }
   }
 
@@ -220,13 +220,13 @@ class InputCodeControl extends ChangeNotifier {
 /// Basic entity to hold decoration of default input look.
 /// Used only when [InputCodeField.builder] and [InputCodeField.itemBuilder] is null.
 class InputCodeDecoration {
-  final Color color;
-  final Color focusColor;
-  final TextStyle textStyle;
-  final Color disableColor;
-  final TextStyle disableTextStyle;
-  final BoxDecoration box;
-  final BoxDecoration focusedBox;
+  final Color? color;
+  final Color? focusColor;
+  final TextStyle? textStyle;
+  final Color? disableColor;
+  final TextStyle? disableTextStyle;
+  final BoxDecoration? box;
+  final BoxDecoration? focusedBox;
   final double width;
   final double height;
   final double focusAlignment;
@@ -254,9 +254,9 @@ class InputCodeField extends StatefulWidget {
   final bool autofocus;
   final TextInputType inputType;
   final TextInputAction inputAction;
-  final IndexedWidgetBuilder itemBuilder;
-  final WidgetBuilder builder;
-  final InputCodeDecoration decoration;
+  final IndexedWidgetBuilder? itemBuilder;
+  final WidgetBuilder? builder;
+  final InputCodeDecoration? decoration;
   final bool enabled;
   final bool obscure;
 
@@ -266,8 +266,8 @@ class InputCodeField extends StatefulWidget {
   /// [builder] custom widget builder - Full control of Widget, return all code fields. Everything is ignored. Use [] operator on [InputCodeControl] to get [char] for field at given index.
   /// [decoration] use to decorate default field style. Used only when [itemBuilder] and [builder] is null.
   InputCodeField({
-    Key key,
-    @required this.control,
+    Key? key,
+    required this.control,
     this.count: 6,
     this.spacing: 8.0,
     this.autofocus: false,
@@ -278,7 +278,7 @@ class InputCodeField extends StatefulWidget {
     this.decoration,
     this.enabled: true,
     this.obscure: false,
-  })  : assert(count != null && count > 0),
+  })  : assert(count > 0),
         super(key: key);
 
   @override
@@ -373,61 +373,40 @@ class _InputCodeFieldState extends State<InputCodeField>
                 .skip(1)
                 .toList(),
           )
-        : widget.builder(context);
+        : widget.builder!(context);
   }
 
   Widget _buildInput(BuildContext context, int index, bool hasFocus) {
-    final theme = Theme.of(context);
     final decoration = this.widget.decoration ?? InputCodeDecoration();
 
     return widget.itemBuilder == null
         ? Flexible(
             fit: decoration.width > 0.0 ? FlexFit.loose : FlexFit.tight,
             child: Container(
-              constraints: BoxConstraints.expand(
-                  width: decoration.width, height: decoration.height),
-              decoration: (hasFocus ? decoration.focusedBox : decoration.box) ??
-                  BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: (hasFocus
-                                ? (decoration.focusColor ??
-                                    theme.primaryColorDark)
-                                : (widget.enabled
-                                    ? (decoration.color ?? theme.primaryColor)
-                                    : (decoration.disableColor ??
-                                        theme.disabledColor)))
-                            .withOpacity(control.hasFocus ? 1.0 : 0.5),
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-              child: Center(
-                child: Text(
-                  (control[index].isNotEmpty && control.isObscured)
-                      ? '•'
-                      : control[index],
-                  style: widget.enabled
-                      ? (decoration.textStyle ??
-                          theme.primaryTextTheme.headline3)
-                      : (decoration.disableTextStyle ??
-                          theme.primaryTextTheme.headline3
-                              .copyWith(color: theme.disabledColor)),
-                ),
-              ),
-            ),
-          )
-        : widget.itemBuilder(context, index);
+                constraints: BoxConstraints.expand(
+                    width: decoration.width, height: decoration.height),
+                decoration: hasFocus ? decoration.focusedBox : decoration.box,
+                child: Center(
+                    child: Text(
+                        (control[index].isNotEmpty &&
+                                control.isObscured != null &&
+                                control.isObscured!)
+                            ? '•'
+                            : control[index],
+                        style: widget.enabled
+                            ? decoration.textStyle
+                            : decoration.disableTextStyle))))
+        : widget.itemBuilder!(context, index);
   }
 
   void _handleFocus(bool hasFocus) {
     if (hasFocus) {
-      if (control._connection == null || !control._connection.attached) {
+      if (control._connection == null || !control._connection!.attached) {
         control._connection = TextInput.attach(this, _inputConfig);
-        control._connection.setEditingState(control._valueCursor);
+        control._connection?.setEditingState(control._valueCursor!);
       }
 
-      control._connection.show();
+      control._connection?.show();
 
       Future.delayed(
         Duration(milliseconds: 150), // wait for keyboard
@@ -452,7 +431,7 @@ class _InputCodeFieldState extends State<InputCodeField>
       control._updateValue(value);
 
   @override
-  TextEditingValue get currentTextEditingValue => control._value;
+  TextEditingValue? get currentTextEditingValue => control._value;
 
   @override
   void connectionClosed() {}
@@ -472,8 +451,23 @@ class _InputCodeFieldState extends State<InputCodeField>
   AutofillScope get currentAutofillScope => throw UnimplementedError();
 
   @override
-  void showAutocorrectionPromptRect(int start, int end) { }
+  void showAutocorrectionPromptRect(int start, int end) {}
 
   @override
-  void performPrivateCommand(String action, Map<String, dynamic> data) { }
+  void performPrivateCommand(String action, Map<String, dynamic> data) {}
+
+  @override
+  void insertTextPlaceholder(Size size) {
+    // TODO
+  }
+
+  @override
+  void removeTextPlaceholder() {
+    // TODO
+  }
+
+  @override
+  void showToolbar() {
+    //TODO
+  }
 }
